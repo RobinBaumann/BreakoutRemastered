d957010a09e3764e97d718369d73a46ceeb73cbb
Robin Baumann <baumann.robin@student.dhbw-karlsruhe.de>
2017-04-10 18:37:27 +0200

    added lobby

--- src/io/github/robinbaumann/breakout/Game.java
+++ src/io/github/robinbaumann/breakout/Game.java
@@ -1,6 +1,7 @@
 package io.github.robinbaumann.breakout;
 
 import io.github.robinbaumann.breakout.views.GamePanel;
+import io.github.robinbaumann.breakout.views.LobbyPanel;
 
 import javax.swing.*;
 
@@ -8,26 +9,43 @@ import javax.swing.*;
  * Project: BreakoutRemastered
  * Created by Robin Baumann on 4/9/17.
  */
-public class Game {
+public class Game  extends JFrame {
     private static final int width = 1280;
     private static final int height = 720;
 
-    public static void main(String[] args) {
-        JFrame gameFrame = new JFrame("Breakout Remastered");
-        GamePanel gamePanel = new GamePanel();
-        gameFrame.setSize(width, height);
-        gameFrame.add(gamePanel);
-        gameFrame.setVisible(true);
-        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public Game(){
+        super("Breakout Remastered");
+    }
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Game gameFrame = new Game();
+            LobbyPanel lobbyPanel = new LobbyPanel(gameFrame);
+            gameFrame.setSize(width, height);
+            gameFrame.add(lobbyPanel);
+            gameFrame.pack();
+            gameFrame.setVisible(true);
+            gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        });
+
+    }
+
+    public void playGame() {
         // Game Loop
+
+        GamePanel gamePanel = new GamePanel();
+
+        this.getContentPane().removeAll();
+        this.getContentPane().invalidate();
+        this.add(gamePanel);
+        this.getContentPane().revalidate();
 
         while (true) {
             gamePanel.move();
             gamePanel.repaint();
             try {
                 Thread.sleep(10);
-            } catch(InterruptedException iEx) {
+            } catch (InterruptedException iEx) {
                 iEx.printStackTrace();
             }
         }
---
+++ src/io/github/robinbaumann/breakout/views/LobbyPanel.java
@@ -0,0 +1,62 @@
+package io.github.robinbaumann.breakout.views;
+
+import io.github.robinbaumann.breakout.Game;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Project: BreakoutRemastered
+ * Created by Robin Baumann on 4/10/17.
+ */
+public class LobbyPanel extends JPanel implements ActionListener {
+
+    private static final JLabel labelForUserTextField = new JLabel("Username");
+    private JTextField userName = new JTextField("", 20);
+    private JButton startButton = new JButton("Play!");
+    private JButton highScoreButton = new JButton("View Best");
+    private JButton cancelButton = new JButton("Cancel");
+    private Game game;
+
+    public LobbyPanel(Game game) {
+
+        startButton.addActionListener(this);
+
+        this.game = game;
+        this.setLayout(new GridBagLayout());
+        this.setBorder(new EmptyBorder(10, 10, 10, 10));
+        GridBagConstraints labelConst = new GridBagConstraints();
+        Dimension preferredSize = new Dimension(1280, 720);
+
+        labelConst.insets = new Insets(3, 3, 3, 3);
+
+        labelConst.gridx = 0;
+        labelConst.gridy = 0;
+        this.add(labelForUserTextField, labelConst);
+        labelConst.gridx = 1;
+        this.add(userName, labelConst);
+        labelConst.gridx = 0;
+        labelConst.gridy = 1;
+        this.add(cancelButton, labelConst);
+        labelConst.gridx = 1;
+        this.add(highScoreButton, labelConst);
+        labelConst.gridx = 2;
+        this.add(startButton, labelConst);
+
+    }
+
+    //TODO: Resize window for play
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        try {
+            if(actionEvent.getSource() == this.startButton) {
+                this.game.playGame();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
